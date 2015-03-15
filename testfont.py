d8d4e0f83b7e17bc51807209346111436930f770
Eric Kelson <ekelson@gmail.com>
2015-03-15 17:45:52 -0400

    font test

---
+++ testfont.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageDraw, ImageFont
+from time import sleep
+from rgbmatrix import Adafruit_RGBmatrix
+
+matrix = Adafruit_RGBmatrix(16, 1)
+image = Image.new('RGB', (32, 16))
+#draw = ImageDraw.Draw(image)
+font=ImageFont.truetype("Ubuntu-r.ttf",10)
+draw.text((10,10),"Test", font=font)
+matrix.SetImage(image.im.id,0,0)
+'''
+x=0
+while x<32:
+	matrix.Clear()
+	#draw.line((x,0,x,15),fill="rgb(255,0,0)")
+	matrix.SetImage(image.im.id,x,0)
+	sleep (1)
+	x=x+1
+'''
+sleep (5)
+matrix.Clear()
